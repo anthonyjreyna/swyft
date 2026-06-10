b2e0070a7d10c47af0f363d426d901f63baf3baf
anthonyjreyna <127710516+anthonyjreyna@users.noreply.github.com>
2026-06-10 11:40:11 -0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Bricolage+Grotesque:opsz,wght@12..96,400;12..96,600;12..96,700&family=Figtree:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
       </Head>
