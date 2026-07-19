685b66e00edd56fad96c15c7818e11bd66507786
anthonyjreyna <127710516+anthonyjreyna@users.noreply.github.com>
2026-07-19 16:14:06 -0700

    Add files via upload

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,12 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Bricolage+Grotesque:opsz,wght@12..96,400;12..96,600;12..96,700&family=Figtree:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "xp498a56ng");`,
+          }}
+        />
       </Head>
       <body>
         <Main />
