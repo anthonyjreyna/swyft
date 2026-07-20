c2cd8b3771babe8a60baf2b2294b73c3f0cbd43f
anthonyjreyna <127710516+anthonyjreyna@users.noreply.github.com>
2026-07-20 11:42:37 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ module.exports = {
       { source: "/talent", destination: "/talent.html" },
       { source: "/talent-video", destination: "/talent-video.html" },
       { source: "/talent-interview", destination: "/talent-interview.html" },
+      { source: "/privacy", destination: "/privacy.html" },
     ];
   },
 };
